8ac914d26030c7a4dbadf1026019cfadd54cf57a
youngchae <010101chae@gmail.com>
2023-08-05 16:39:53 +0900

    feat: todo 수정 구현

--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -6,6 +6,8 @@ import todoItemType from "../types/todoItem";
 const Todo = () => {
   const [todoInput, setTodoInput] = useState("");
   const [todoItems, setTodoItems] = useState<todoItemType[]>([]);
+  const [modifyingItem, setModifyingItem] = useState(-1);
+  const [modifyInput, setModifyInput] = useState("");
 
   const getTodos = async () => {
     try {
@@ -60,6 +62,40 @@ const Todo = () => {
     }
   };
 
+  const handleModifyInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length >= 20) {
+      alert("최대 20자까지 입력할 수 있습니다.");
+    }
+    setModifyInput(e.target.value);
+  };
+
+  const updateTodoValue = async (
+    e: React.FormEvent<HTMLFormElement>,
+    id: number
+  ) => {
+    e.preventDefault();
+    const targetItem = todoItems.find((item) => item.id === id);
+    try {
+      const response = await fetch(`${API_BASE_URL}/todos/${id}`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          todo: modifyInput,
+          isCompleted: targetItem?.isCompleted,
+        }),
+      });
+      if (response.status === 200) {
+        setModifyInput("");
+        setModifyingItem(-1);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const deleteTodo = async (id: number) => {
     try {
       const response = await fetch(`${API_BASE_URL}/todos/${id}`, {
@@ -78,7 +114,7 @@ const Todo = () => {
 
   useEffect(() => {
     getTodos();
-  }, []);
+  }, [modifyingItem]);
 
   return (
     <>
@@ -99,19 +135,65 @@ const Todo = () => {
           {todoItems
             .map((item) => (
               <li key={item.id}>
-                <label>
-                  <input type="checkbox" defaultChecked={item.isCompleted} />
-                  <span>{item.todo}</span>
-                </label>
-                <div>
-                  <button data-testid="modify-button">수정</button>
-                  <button
-                    data-testid="delete-button"
-                    onClick={() => deleteTodo(item.id)}
+                {modifyingItem === item.id ? (
+                  <ModifyForm
+                    onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
+                      updateTodoValue(e, item.id)
+                    }
                   >
-                    삭제
-                  </button>
-                </div>
+                    <label>
+                      <input
+                        type="checkbox"
+                        defaultChecked={item.isCompleted}
+                      />
+                      <ModifyInput
+                        type="text"
+                        maxLength={20}
+                        width={"100%"}
+                        defaultValue={item.todo}
+                        name="modifyInput"
+                        onChange={handleModifyInput}
+                      />
+                    </label>
+                    <div>
+                      <button type="submit">완료</button>
+                      <button
+                        onClick={() => {
+                          setModifyingItem(-1);
+                        }}
+                      >
+                        취소
+                      </button>
+                    </div>
+                  </ModifyForm>
+                ) : (
+                  <TodoItem>
+                    <label>
+                      <input
+                        type="checkbox"
+                        defaultChecked={item.isCompleted}
+                      />
+                      <span>{item.todo}</span>
+                    </label>
+                    <div>
+                      <button
+                        data-testid="modify-button"
+                        onClick={() => {
+                          setModifyingItem(item.id);
+                          setModifyInput(item.todo);
+                        }}
+                      >
+                        수정
+                      </button>
+                      <button
+                        data-testid="delete-button"
+                        onClick={() => deleteTodo(item.id)}
+                      >
+                        삭제
+                      </button>
+                    </div>
+                  </TodoItem>
+                )}
               </li>
             ))
             .reverse()}
@@ -162,12 +244,6 @@ const TodoList = styled.ul`
   padding: 0;
   list-style: none;
 
-  li {
-    margin: 4px 0;
-    display: flex;
-    justify-content: space-between;
-  }
-
   label {
     display: flex;
     align-items: center;
@@ -189,4 +265,37 @@ const TodoList = styled.ul`
     margin: 0 4px;
     cursor: pointer;
   }
+`;
+const TodoItem = styled.div`
+  margin: 4px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ModifyForm = styled.form`
+  margin: 4px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  label {
+    display: flex;
+    align-items: center;
+    flex: 1;
+    cursor: pointer;
+  }
+  button {
+    font-size: 20px;
+    margin: 0 4px;
+    cursor: pointer;
+  }
+`;
+
+const ModifyInput = styled.input`
+  font-size: 20px;
+  color: #888888;
+  flex: 1;
+  padding: 4px;
+  margin: 0 10px;
 `;
